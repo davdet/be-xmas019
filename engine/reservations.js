3bf7f231b5324156c0a138ef8a120efe43f00102
dev-dd <dv.dt@hotmail.com>
2020-01-03 17:42:24 +0100

    Come precedente

--- engine/reservations.js
+++ engine/reservations.js
@@ -34,6 +34,27 @@ const getReservationByEmail = async (req, res) => {
   return res.status(200).send(reservation);
 };
 
+const getReservationById = async (req, res) => {
+  const reservationId = req.params.idReservation; //Recupero l'email passata come parametro
+  let reservation;
+
+  try {
+    reservation = await Reservations.findOne({where: {idReservation: reservationId}});
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+
+  if (!reservation) { //Nel caso in cui non sia stata trovata alcuna prenotazione (reservation sarà null) restituisco come risposta un errore
+    return res.status(404).send({
+      error: true, //Posso aggiungere info alla mia risposta indicando maggiori info sull'errore
+      message: 'The reservation requested does not exist.',
+      reservationId
+    })
+  }
+  //In caso contrario restituisco la prenotazione
+  return res.status(200).send(reservation);
+};
+
 const addReservation = async (req, res) => {
   const bodyReservation = req.body;
   let reservation;
@@ -49,22 +70,22 @@ const addReservation = async (req, res) => {
 };
 
 const editReservation = async (req, res) => {
-  const emailReservation = req.params.email;    //rimettere idReservation al posto di email
+  const idReserv = req.params.idReservation;    //rimettere idReservation al posto di email
   const reservation = req.body;
   let reservationFound, updated;
 
   try {
-    reservationFound = await Reservations.findOne({where: {email: emailReservation}});
+    reservationFound = await Reservations.findOne({where: {idReservation: idReserv}});
 
     if (!reservationFound) {
       return res.status(404).send({
         error: true,
         message: 'Cannot update a reservation that does not exist.',
-        emailReservation
+        idReserv
       })
     }
 
-    updated = await Reservations.update({...reservation}, {where: {email: emailReservation}});
+    updated = await Reservations.update({...reservation}, {where: {idReservation: idReserv}});
 
   } catch (error) {
     return res.status(500).send(error);
@@ -75,12 +96,12 @@ const editReservation = async (req, res) => {
 
     ? res.status(201).send({ //Nel caso in cui tutto sia andato a buon fine e trovo quindi un elemento allora invio una risposta di esito positivo
       updated: true,
-      emailReservation
+      idReserv
     })
 
     : res.status(400).send({ //Nel caso qualcosa non sia andato bene allora restituisco un codice di errore (400 codice di stato HTTP: la richiesta non può essere soddisfatta a causa di errori di sintassi) 
       updated: false,
-      emailReservation
+      idReserv
     });
 };
 
@@ -101,6 +122,7 @@ const deleteReservation = async (req, res) => {
 module.exports = {
   getReservations,
   getReservationByEmail,
+  getReservationById,
   editReservation,
   deleteReservation,
   addReservation
